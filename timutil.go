3caed6114ce1edab4142083bf912a2c297322ce3
nico <brun.nicolas21@gmail.com>
2019-03-25 09:08:36 +0100

    compute delta between GPS and UNIX epoch

--- timutil.go
+++ timutil.go
@@ -1,50 +1,54 @@
 package timutil
 
 import (
-  "time"
-  "sort"
+	"sort"
+	"time"
 )
 
-var dates = []time.Time {
-  time.Date(1981, 6, 30, 0, 0, 0, 0, time.UTC),
-  time.Date(1982, 6, 30, 0, 0, 0, 0, time.UTC),
-  time.Date(1983, 6, 30, 0, 0, 0, 0, time.UTC),
-  time.Date(1985, 6, 30, 0, 0, 0, 0, time.UTC),
-  time.Date(1987, 12, 31, 0, 0, 0, 0, time.UTC),
-  time.Date(1989, 12, 31, 0, 0, 0, 0, time.UTC),
-  time.Date(1990, 12, 31, 0, 0, 0, 0, time.UTC),
-  time.Date(1992, 6, 30, 0, 0, 0, 0, time.UTC),
-  time.Date(1993, 6, 30, 0, 0, 0, 0, time.UTC),
-  time.Date(1994, 6, 30, 0, 0, 0, 0, time.UTC),
-  time.Date(1995, 12, 31, 0, 0, 0, 0, time.UTC),
-  time.Date(1997, 6, 30, 0, 0, 0, 0, time.UTC),
-  time.Date(1998, 12, 31, 0, 0, 0, 0, time.UTC),
-  time.Date(2005, 12, 31, 0, 0, 0, 0, time.UTC),
-  time.Date(2008, 12, 31, 0, 0, 0, 0, time.UTC),
-  time.Date(2012, 6, 30, 0, 0, 0, 0, time.UTC),
-  time.Date(2015, 6, 30, 0, 0, 0, 0, time.UTC),
-  time.Date(2016, 12, 31, 0, 0, 0, 0, time.UTC),
+var dates = []time.Time{
+	time.Date(1981, 6, 30, 0, 0, 0, 0, time.UTC),
+	time.Date(1982, 6, 30, 0, 0, 0, 0, time.UTC),
+	time.Date(1983, 6, 30, 0, 0, 0, 0, time.UTC),
+	time.Date(1985, 6, 30, 0, 0, 0, 0, time.UTC),
+	time.Date(1987, 12, 31, 0, 0, 0, 0, time.UTC),
+	time.Date(1989, 12, 31, 0, 0, 0, 0, time.UTC),
+	time.Date(1990, 12, 31, 0, 0, 0, 0, time.UTC),
+	time.Date(1992, 6, 30, 0, 0, 0, 0, time.UTC),
+	time.Date(1993, 6, 30, 0, 0, 0, 0, time.UTC),
+	time.Date(1994, 6, 30, 0, 0, 0, 0, time.UTC),
+	time.Date(1995, 12, 31, 0, 0, 0, 0, time.UTC),
+	time.Date(1997, 6, 30, 0, 0, 0, 0, time.UTC),
+	time.Date(1998, 12, 31, 0, 0, 0, 0, time.UTC),
+	time.Date(2005, 12, 31, 0, 0, 0, 0, time.UTC),
+	time.Date(2008, 12, 31, 0, 0, 0, 0, time.UTC),
+	time.Date(2012, 6, 30, 0, 0, 0, 0, time.UTC),
+	time.Date(2015, 6, 30, 0, 0, 0, 0, time.UTC),
+	time.Date(2016, 12, 31, 0, 0, 0, 0, time.UTC),
 }
 
 const FormatDate = "2006-01-02T15:04:05.000"
 
 var (
-  UNIX = time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)
-  GPS  = time.Date(1980, 1, 6, 0, 0, 0, 0, time.UTC)
-  delta = GPS.Sub(UNIX)
+	UNIX  = time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)
+	GPS   = time.Date(1980, 1, 6, 0, 0, 0, 0, time.UTC)
+	delta = GPS.Sub(UNIX)
 )
 
 func init() {
-  sort.Slice(dates, func(i, j int) bool {
-    return dates[i].Before(dates[j])
-  })
+	sort.Slice(dates, func(i, j int) bool {
+		return dates[i].Before(dates[j])
+	})
+}
+
+func DeltaGPS() time.Duration {
+	return GPS.Sub(UNIX)
 }
 
 func GPSTime(t time.Time, epoch bool) time.Time {
-  if epoch {
-    t = t.Add(-delta)
-  }
-  return t.Add(leap(t))
+	if epoch {
+		t = t.Add(-delta)
+	}
+	return t.Add(leap(t))
 }
 
 func Split5(t time.Time) (uint32, uint8) {
@@ -55,10 +59,10 @@ func Split5(t time.Time) (uint32, uint8) {
 }
 
 func Split6(t time.Time) (uint32, uint16) {
-  s := uint32(t.Unix())
-  m := float64(t.UnixNano())/1000000.0
+	s := uint32(t.Unix())
+	m := float64(t.UnixNano()) / 1000000.0
 
-  return s, uint16(m)
+	return s, uint16(m)
 }
 
 func Join5(coarse uint32, fine uint8) time.Time {
@@ -79,17 +83,17 @@ func Join6(coarse uint32, fine uint16) time.Time {
 }
 
 func gpsTime(t time.Time) time.Time {
-  return t.UTC().Add(-delta+leap(t))
+	return t.UTC().Add(-delta + leap(t))
 }
 
 func utcTime(t time.Time) time.Time {
-  return t.Add(delta).UTC()
-  // return t.Add(-leap(t))
+	return t.Add(delta).UTC()
+	// return t.Add(-leap(t))
 }
 
 func leap(t time.Time) time.Duration {
-  i := sort.Search(len(dates), func(i int) bool {
-    return t.Before(dates[i]) || t.Equal(dates[i])
-  })
-  return time.Duration(i) * time.Second
+	i := sort.Search(len(dates), func(i int) bool {
+		return t.Before(dates[i]) || t.Equal(dates[i])
+	})
+	return time.Duration(i) * time.Second
 }
--- timutil_test.go
+++ timutil_test.go
@@ -1,13 +1,13 @@
 package timutil
 
 import (
-  "time"
-  "fmt"
+	"fmt"
+	"time"
 )
 
 func ExampleGPSTime() {
-  t := time.Date(2019, 2, 21, 8, 36, 12, 0, time.UTC)
-  g := GPSTime(t)
-  fmt.Println(g.Format("2006-01-02 15:04:05"))
-  // Output: 2019-02-21 08:36:30
+	t := time.Date(2019, 2, 21, 8, 36, 12, 0, time.UTC)
+	g := GPSTime(t)
+	fmt.Println(g.Format("2006-01-02 15:04:05"))
+	// Output: 2019-02-21 08:36:30
 }
